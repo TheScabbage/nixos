5f3ece1b00fffb04d6b9124cc62ba0d96a306884
scabbage <thescabbage@gmail.com>
2024-02-21 22:40:11 +0800

    Enable docker virtualisation

--- bytebox/configuration.nix
+++ bytebox/configuration.nix
@@ -137,6 +137,7 @@
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
   services.blueman.enable = true;
+  virtualisation.docker.enable = true;
   # Meat and potatoes
   environment.systemPackages = with pkgs; [
     neovim
