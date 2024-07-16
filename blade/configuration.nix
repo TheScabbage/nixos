c305762ff5a6a02c56f6fbd9cca0b83ac37cfd86
scabbage <thescabbage@gmail.com>
2024-07-16 14:47:14 +0800

    Use kernel 6.9

--- blade/configuration.nix
+++ blade/configuration.nix
@@ -13,7 +13,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  #boot.kernelPackages = pkgs.linuxKernel.kernels.linux_6_7;
+  boot.kernelPackages = pkgs.linuxPackages_6_9;
  
   networking.hostName = "nixbook"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
