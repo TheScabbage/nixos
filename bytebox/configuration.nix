0adbbccdd0a7b4643af208b9694589470be4d986
scabbage <thescabbage@gmail.com>
2024-07-22 10:22:28 +0800

    Move stateful data variable to top of config file

--- bytebox/configuration.nix
+++ bytebox/configuration.nix
@@ -6,6 +6,9 @@
       ./hardware-configuration.nix
     ];
 
+  # Settings for stateful data, like file locations and database versions
+  system.stateVersion = "23.05";
+
   networking.hostName = "bytebox"; 
   networking.nameservers = [ "9.9.9.9" "1.1.1.1" ];
 
@@ -389,10 +392,4 @@ fonts.packages = with pkgs; [
   helvetica-neue-lt-std
 ];
 
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05";
 }
