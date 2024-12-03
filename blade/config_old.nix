72e3776e84f699939969ebf259fc442abc20b143
scabbage <thescabbage@gmail.com>
2024-12-03 20:58:17 +0800

    save old config

---
+++ blade/config_old.nix
@@ -0,0 +1,295 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ];
+
+# Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+#boot.kernelPackages = pkgs.linuxKernel.kernels.linux_6_7;
+
+  networking.hostName = "nixbook"; # Define your hostname.
+# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+# Configure network proxy if necessary
+# networking.proxy.default = "http://user:password@proxy:port/";
+# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+# Enable networking
+    networking.networkmanager.enable = true;
+  networking.nameservers = ["1.1.1.1" "9.9.9.9"];
+
+# Set your time zone.
+  time.timeZone = "Pacific/Auckland";
+
+# Select internationalisation properties.
+  i18n.defaultLocale = "en_AU.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_AU.UTF-8";
+    LC_IDENTIFICATION = "en_AU.UTF-8";
+    LC_MEASUREMENT = "en_AU.UTF-8";
+    LC_MONETARY = "en_AU.UTF-8";
+    LC_NAME = "en_AU.UTF-8";
+    LC_NUMERIC = "en_AU.UTF-8";
+    LC_PAPER = "en_AU.UTF-8";
+    LC_TELEPHONE = "en_AU.UTF-8";
+    LC_TIME = "en_AU.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  programs.dconf.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # NVIDIA 
+  # Make sure opengl is enabled
+  hardware.graphics.enable = true;
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  hardware.nvidia.prime = {
+    offload = {
+      enable = true;
+      enableOffloadCmd = true;
+    };
+
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:01:0:0";
+    # amdgpuBusId = "PCI:54:0:0"; For AMD GPU
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.scabbage = {
+    isNormalUser = true;
+    description = "Scabbage";
+    extraGroups = [ "networkmanager" "wheel" "openrazer" "docker" "audio" "plugdev" "fuse"];
+    packages = with pkgs; [
+      firefox
+        chromium
+        kate
+    ];
+  };
+
+
+# Shell aliases
+  programs.bash.shellAliases = {
+    l = "ls -alh";
+    cfg = "nvim $HOME/nixos/blade/configuration.nix";
+    nbs = "sudo nixos-rebuild switch -I nixos-config=$HOME/nixos/blade/configuration.nix";
+    try = "nix-shell -p ";
+    ns = "nix-shell";
+    vi = "nvim ";
+    vd = "WINIT_UNIX_BACKEND=x11 neovide ";
+    lg = "lazygit";
+    cr = "clear";
+    sd = "pwd > ~/.local/jmp/location.txt";
+    jd = "JMP_DIR=`cat ~/.local/jmp/location.txt` && cd $JMP_DIR && pwd";
+    nvo = "~/projects/nvidia-offload/nvidia-offload.sh ";
+  };
+
+
+# Enable automatic login for the user.
+#services.xserver.displayManager.autoLogin.enable = true;
+#services.xserver.displayManager.autoLogin.user = "scabbage";
+
+# Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+
+# Flatpak 
+  services.flatpak.enable = true;
+
+# List packages installed in system profile. To search, run:
+# $ nix search wget
+  environment.systemPackages = with pkgs; [
+
+    # Tools
+    neovim
+    tmux
+    kitty
+    git
+    wget
+    bash
+    curl
+    unzip
+    yabridge
+    yabridgectl
+    wine-staging
+    bitwarden
+    barrier
+    openssl
+    ffmpeg_6-full 
+    mpv
+    fuse
+    openjdk17-bootstrap
+    rstudio
+    stow
+    lazygit
+    zig
+    cargo
+    gopls
+    ripgrep
+    nodejs_22
+    nodePackages.typescript-language-server
+    tree-sitter
+    pkg-config
+    cmake
+    libgcc
+    gcc9
+    python3
+    libGL
+    libglibutil
+    btop
+    wl-clipboard
+    ninja
+
+    # Apps
+    neovide
+    polychromatic
+    skypeforlinux
+    discord
+    vscode
+    reaper
+    freecad
+    prusa-slicer
+    inkscape
+    freecad
+    steam-run
+    gparted
+    prismlauncher
+
+    # Drivers n shit
+    ntfs3g
+    exfatprogs
+
+    # Touchpad
+    touchegg
+
+    # VM
+    qemu
+    virt-manager
+    virt-viewer
+    dnsmasq
+    vde2
+    iproute2
+    netcat-openbsd
+    libguestfs
+  ];
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
+
+
+  environment.variables = rec {
+    MOZ_USE_XINPUT2="1";
+  };
+
+  # Razer Support
+  hardware.openrazer.enable = true;
+
+  # Bluetooth
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+  
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  #networking.firewall.allowedTCPPorts = [ ... ];
+  #networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+}
