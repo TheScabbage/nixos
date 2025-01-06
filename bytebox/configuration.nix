17ff969b3e798cefaad3f252a1fbf0b8f2c97a7a
scabbage <thescabbage@gmail.com>
2025-01-06 14:28:07 +0800

    Update sound nix config

--- bytebox/configuration.nix
+++ bytebox/configuration.nix
@@ -85,9 +85,8 @@
   #};
 
   # Enable sound with pipewire.
-  sound.enable = true;
   nixpkgs.config.pulseaudio = true;
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
