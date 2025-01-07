bae192f8185bb9db9dd4fa2fe9eb6c937dbf2df9
scabbage <thescabbage@gmail.com>
2025-01-07 22:36:06 +0800

    Fix syntax error in `flake.nix`

--- bytebox/flake.nix
+++ bytebox/flake.nix
@@ -15,7 +15,7 @@
             bytebox = lib.nixosSystem {
                 system = "x86_64-linux";
                 modules = [ ./configuration.nix ];
-            }
+            };
         };
     };
 }
