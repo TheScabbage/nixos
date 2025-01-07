4dddc615c45370a9f1a85ea58c54fe4a693da67a
scabbage <thescabbage@gmail.com>
2025-01-07 22:34:13 +0800

    Add flake.nix file for `bytebox`

---
+++ bytebox/flake.nix
@@ -0,0 +1,21 @@
+{
+    description = "Scabbageflake";
+
+    # Some git repos
+    inputs = {
+        nixpkgs.url = "nixpkgs/nixos-24.11";
+    };
+
+    # System config
+    outputs = { self, nixpkgs, ...}:
+    let
+        lib = nixpkgs.lib;
+    in {
+        nixosConfigurations = {
+            bytebox = lib.nixosSystem {
+                system = "x86_64-linux";
+                modules = [ ./configuration.nix ];
+            }
+        };
+    };
+}
