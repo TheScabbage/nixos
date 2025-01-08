101148110205e206ed9addca9b7ee016083b3788
scabbage <thescabbage@gmail.com>
2025-01-08 13:58:46 +0800

    Swap sddm for lightdm and set wallpaper

--- bytebox/sys/de/kde.nix
+++ bytebox/sys/de/kde.nix
@@ -2,15 +2,18 @@
 { config, pkgs, ... }:
 {
   services.xserver.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.displayManager.lightdm.background = ./wallpapers/space.jpg;
   services.desktopManager.plasma6.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.theme = "where-is-my-sddm-theme";
+  #services.displayManager.sddm.enable = true;
+  #services.displayManager.sddm.theme = "tokyo-night-sddm";
 
   xdg.portal.enable = true;
   xdg.portal.xdgOpenUsePortal = true;
 
   environment.systemPackages = with pkgs; [
     kdePackages.kwalletmanager
+    #kdePackages.sddm
     xwayland
     xwaylandvideobridge
     xdg-desktop-portal-kde
