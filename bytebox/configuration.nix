0296050043aa6a3c441ef29ebdd9f84bea8b523b
scabbage <thescabbage@gmail.com>
2025-02-12 05:27:22 +0800

    Enable opencl/vlk

--- bytebox/configuration.nix
+++ bytebox/configuration.nix
@@ -110,6 +110,8 @@
   # Graphics
   hardware.graphics.enable = true;
   hardware.graphics.enable32Bit = true; 
+  hardware.amdgpu.opencl.enable = true;
+  hardware.amdgpu.amdvlk.enable = true;
 
   # OpenSSH
   services.openssh.enable = true;
