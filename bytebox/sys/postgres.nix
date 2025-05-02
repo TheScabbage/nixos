61e5e6aeca1dd0462464ae7d54c859dc43445c1e
scabbage <thescabbage@gmail.com>
2025-05-02 19:19:39 +0800

    Disable postgres

--- bytebox/sys/postgres.nix
+++ bytebox/sys/postgres.nix
@@ -1,14 +1,14 @@
 { config, pkgs, ... }:
 {
-    services.postgresql = {
-        enable = true;
-        ensureDatabases = [ "dust" ];
-        authentication = pkgs.lib.mkOverride 10 ''
-            #type DATABASE  USER        ADDRESS     METHOD
-            local all       all                     trust
-            host  all       postgres    0.0.0.0/0   scram-sha-256
-        '';
-    };
+    #services.postgresql = {
+    #    enable = true;
+    #    ensureDatabases = [ "dust" ];
+    #    authentication = pkgs.lib.mkOverride 10 ''
+    #        #type DATABASE  USER        ADDRESS     METHOD
+    #        local all       all                     trust
+    #        host  all       postgres    0.0.0.0/0   scram-sha-256
+    #    '';
+    #};
 
     environment.systemPackages = with pkgs; [
         pgadmin4
