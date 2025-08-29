163871bee6c363fb241f204668c302f827d5f9fa
scabbage <thescabbage@gmail.com>
2025-08-30 00:56:45 +0800

    Fmt config

--- blade/configuration.nix
+++ blade/configuration.nix
@@ -9,20 +9,20 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
-  
+
   boot.loader.grub.useOSProber = true;
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_6_12;
- 
+
   networking.hostName = "nixbook"; # Define your hostname.
   #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
- 
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
- 
+
   # Enable networking
   networking.networkmanager.enable = true;
   networking.nameservers = ["1.1.1.1" "9.9.9.9"];
