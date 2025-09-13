e500b8f4fe80bdaad4503257fb3ef5e744c1224e
scabbage <thescabbage@gmail.com>
2025-09-13 17:10:31 +0800

    Pin `unityhub` overlay

--- bytebox/flake.nix
+++ bytebox/flake.nix
@@ -1,22 +1,33 @@
 {
-    description = "Scabbageflake";
+  description = "Scabbageflake";
 
-    # Some git repos
-    inputs = {
-        #nixpkgs.url = "nixpkgs/nixos-24.11";
-        nixpkgs.url = "nixpkgs/nixos-unstable";
-    };
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs-roslyn.url = "github:NixOS/nixpkgs/a595dde4d0d3";
+    nixpkgs-unityhub.url = "github:NixOS/nixpkgs/6fdcbf8463e";
+  };
 
-    # System config
-    outputs = { self, nixpkgs, ...}:
-    let
-        lib = nixpkgs.lib;
-    in {
-        nixosConfigurations = {
-            bytebox = lib.nixosSystem {
-                system = "x86_64-linux";
-                modules = [ ./configuration.nix ];
-            };
-        };
+  outputs = { self, nixpkgs, nixpkgs-roslyn, ... }:
+  let
+    lib = nixpkgs.lib;
+  in {
+    nixosConfigurations = {
+      bytebox = lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          {
+            nixpkgs.overlays = [
+              (final: prev: {
+                roslyn-ls = nixpkgs-roslyn.legacyPackages.${prev.system}.roslyn-ls;
+              })
+              (final: prev: {
+                unityhub = nixpkgs-unityhub.legacyPackages.${prev.system}.unityhub;
+              })
+            ];
+          }
+        ];
+      };
     };
+  };
 }
