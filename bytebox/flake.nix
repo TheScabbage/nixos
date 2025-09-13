0de2349e4a362a166900ba972bb750bffef2cbe0
scabbage <thescabbage@gmail.com>
2025-09-13 17:34:43 +0800

    Update flake

--- bytebox/flake.nix
+++ bytebox/flake.nix
@@ -4,10 +4,10 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     nixpkgs-roslyn.url = "github:NixOS/nixpkgs/a595dde4d0d3";
-    nixpkgs-unityhub.url = "github:NixOS/nixpkgs/6fdcbf8463e";
+    nixpkgs-unityhub.url = "github:NixOS/nixpkgs/517841072fbf8de9ac8f305c1340ce8fcf764a5f";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-roslyn, ... }:
+  outputs = { self, nixpkgs, nixpkgs-roslyn, nixpkgs-unityhub, ... }:
   let
     lib = nixpkgs.lib;
   in {
