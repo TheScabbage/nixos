bbbd2de9df4286257c864c9e4754920ddea32548
scabbage <thescabbage@gmail.com>
2025-10-06 01:54:43 +0800

    Disable amdvlk

--- bytebox/configuration.nix
+++ bytebox/configuration.nix
@@ -130,7 +130,7 @@
   hardware.graphics.enable = true;
   hardware.graphics.enable32Bit = true;
   hardware.amdgpu.opencl.enable = true;
-  hardware.amdgpu.amdvlk.enable = true;
+  #hardware.amdgpu.amdvlk.enable = true;
 
   # OpenSSH
   services.openssh.enable = true;
