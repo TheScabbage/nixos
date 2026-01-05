c8a898ff8f1b580942efd402242f4e90974e0829
scabbage <thescabbage@gmail.com>
2026-01-05 13:44:23 +0800

    Remove `vi` alias

--- bytebox/sys/shell/fish.nix
+++ bytebox/sys/shell/fish.nix
@@ -6,14 +6,14 @@
   programs.fish.shellAliases = {
     l = "eza -alh";
     lg = "lazygit";
-    cfg = "pushd . && cd $HOME/.config/nixos/bytebox/ && vi ./configuration.nix && popd";
+    cfg = "pushd . && cd $HOME/.config/nixos/bytebox/ && nvim ./configuration.nix && popd";
     #nbs = "sudo nixos-rebuild switch -I nixos-config=$HOME/.config/nixos/bytebox/configuration.nix";
     nbs = "pushd . && cd $HOME/.config/nixos/bytebox/ && sudo nixos-rebuild switch --flake . && popd";
     nuf = "pushd . && cd $HOME/.config/nixos/bytebox/ && nix flake update && popd";
     ndg = "sudo nix-collect-garbage --delete-older-than 7d";
     try = "nix-shell -p ";
     search = "nix search nixpkgs ";
-    vi  = "nvim";
+    v   = "nvim";
     vw  = "neovide . &";
     sd = "pwd > ~/.local/share/jumpdir";
     jd = "cd $(cat ~/.local/share/jumpdir)";
