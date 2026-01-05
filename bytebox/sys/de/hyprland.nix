230a5f6b59b8239eb5ae714b689b5541747f5f0c
scabbage <thescabbage@gmail.com>
2026-01-05 13:43:55 +0800

    Fix borked hyprland config

--- bytebox/sys/de/hyprland.nix
+++ bytebox/sys/de/hyprland.nix
@@ -1,18 +1,30 @@
 { config, pkgs, ... }:
 {
     programs.hyprland.enable = true;
+    networking.networkmanager.enable = true;
+    services.displayManager.sddm.autoNumlock = true;
 
     security.pam.services.login.enableGnomeKeyring = true;
+    security.pam.services.sddm.enableGnomeKeyring = true;
+    security.polkit.enable = true;
+
+    services.gnome.gnome-keyring.enable = true;
 
     environment.systemPackages = with pkgs; [
         kitty
+        gnome-keyring
+        swww
+        polkit_gnome
         waybar
         networkmanagerapplet
         wofi
         hyprpaper
         hyprshot
-        gnome-keyring
+        hyprlandPlugins.hyprexpo
+        hyprlandPlugins.hyprwinwrap
+        xwayland
         xdg-desktop-portal-hyprland
+        playerctl
         xfce.thunar
         lxappearance
         nwg-look
--- bytebox/sys/de/kde.nix
+++ bytebox/sys/de/kde.nix
@@ -5,6 +5,21 @@
   xdg.portal.enable = true;
   xdg.portal.xdgOpenUsePortal = true;
 
+  system.userActivationScripts = {
+      startXdgDesktopPortal.text = ''
+        #!/bin/sh
+        source ${config.system.build.setEnvironment}
+
+        systemctl --user stop xdg-desktop-portal-hyprland
+        systemctl --user mask xdg-desktop-portal-hyprland
+
+        systemctl --user unmask xdg-desktop-portal-kde
+        systemctl --user start xdg-desktop-portal-kde
+
+        systemctl --user start xdg-desktop-portal
+      '';
+  };
+
   environment.systemPackages = with pkgs; [
     kdePackages.kwallet
     kdePackages.kwalletmanager
