45e2287f4f8ad875bad5a053bf3e3e90c5e42246
scabbage <thescabbage@gmail.com>
2026-02-21 10:36:39 +0800

    Disable forgejo

--- bytebox/configuration.nix
+++ bytebox/configuration.nix
@@ -82,6 +82,54 @@
   # Enable sound with pipewire.
   security.rtkit.enable = true;
 
+  #services.forgejo = {
+  #  enable = true;
+  #  database.type = "sqlite3";
+  #  # Enable support for Git Large File Storage
+  #  lfs.enable = true;
+  #  settings = {
+  #    #server = {
+  #    #  DOMAIN = "git.example.com";
+  #    #  # You need to specify this to remove the port from URLs in the web UI.
+  #    #  ROOT_URL = "https://${srv.DOMAIN}/"; 
+  #    #  HTTP_PORT = 3000;
+  #    #};
+  #    # You can temporarily allow registration to create an admin user.
+  #    service.DISABLE_REGISTRATION = false;
+  #    # Add support for actions, based on act: https://github.com/nektos/act
+  #    actions = {
+  #      ENABLED = true;
+  #      DEFAULT_ACTIONS_URL = "github";
+  #    };
+  #    # Sending emails is completely optional
+  #    # You can send a test email from the web UI at:
+  #    # Profile Picture > Site Administration > Configuration >  Mailer Configuration 
+  #    #mailer = {
+  #    #  ENABLED = true;
+  #    #  SMTP_ADDR = "mail.example.com";
+  #    #  FROM = "noreply@${srv.DOMAIN}";
+  #    #  USER = "noreply@${srv.DOMAIN}";
+  #    #};
+  #  };
+  #  #secrets = {
+  #  #  mailer.PASSWD = config.age.secrets.forgejo-mailer-password.path;
+  #  #};
+  #};
+
+  #services.gitea-actions-runner = {
+  #  package = pkgs.forgejo-runner;
+  #  #stateDir = "/home/scabbage/gitea";
+  #  instances.default = {
+  #    enable = true;
+  #    name = "local-runner";
+  #    url = "http://localhost:3000";
+  #    tokenFile = "/etc/forgejo/token";
+  #    labels = [
+  #      "ubuntu-latest:docker://node:20-bookworm"
+  #    ];
+  #  };
+  #};
+
   services.pipewire = {
     enable = true;
     #wireplumber.enable = true;
