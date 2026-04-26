18a11f20ff552adcb2a063cf198afa5f8ccf9a1a
scabbage <thescabbage@gmail.com>
2026-04-27 03:02:12 +0800

    Disable xone

--- bytebox/sys/game.nix
+++ bytebox/sys/game.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... }:
 {
-  boot.extraModulePackages = with config.boot.kernelPackages; [ xpadneo xone ];
+  boot.extraModulePackages = with config.boot.kernelPackages; [ 
+    xpadneo 
+    #xone
+  ];
 
   # Use steam-hardware module for KK3 nintendo switch mode
   hardware.steam-hardware.enable = true;
@@ -11,7 +14,7 @@
   boot.extraModprobeConfig = '' options bluetooth disable_ertm=1 '';
 
   # Xbox wireless adapter
-  hardware.xone.enable = true;
+  #hardware.xone.enable = true;
 
   # Fixes Finals crashing on startup
   #boot.kernelParams = [ "clearcpuid=304" ];
